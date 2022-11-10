4904bd83c22a55bd68d6a27be56789c8de7b08bc
Hawre Salih <106107046+Hawre1987@users.noreply.github.com>
2022-11-10 08:41:42 +0300

    Add files via upload

---
+++ DataTableFromJson/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Data;
+
+namespace DataTableFromJson
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var filePath = new StreamReader("../../../items.json");
+            JObject jsonObject = JObject.Parse(filePath.ReadToEnd());
+            var items = JsonConvert.SerializeObject(jsonObject["items"]);
+            DataTable tester = (DataTable)JsonConvert.DeserializeObject(items, (typeof(DataTable)));
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
